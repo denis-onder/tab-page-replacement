fcb3e905c7000d12b00823b1a56e66019afa3e81
Denis Onder <denis_onder@protonmail.com>
2019-10-11 16:58:00 +0200

    Highlight sidebar icons

--- src/components/layout/sidebar/index.js
+++ src/components/layout/sidebar/index.js
@@ -1,26 +1,43 @@
 import React from "react";
 import "./styles.scss";
 
+const activeLink = e => {
+  const elements = document.getElementsByClassName("sidebar_list_item");
+  const target = document.getElementById(e.currentTarget.id);
+  target.classList.toggle("active");
+  for (let i = 0; i < elements.length; i++) {
+    if (elements[i] !== target) elements[i].classList.remove("active");
+  }
+};
+
 export default function Sidebar() {
   return (
     <div id="sidebar">
       <ul className="sidebar_list">
-        <li className="sidebar_list_item">
-          <i className="sidebar_list_item_icon fab fa-github"></i>
-        </li>
-        <li className="sidebar_list_item">
-          <i className="sidebar_list_item_icon fab fa-stack-overflow"></i>
-        </li>
-        <li className="sidebar_list_item">
-          <i className="sidebar_list_item_icon fab fa-dev"></i>
-        </li>
-        <li className="sidebar_list_item">
-          <i className="sidebar_list_item_icon fab fa-linkedin"></i>
-        </li>
-        <li className="sidebar_list_item">
+        <a href="https://github.com" className="sidebar_list_link">
+          <li className="sidebar_list_item">
+            <i className="sidebar_list_item_icon fab fa-github"></i>
+          </li>
+        </a>
+        <a href="https://stackoverflow.com/" className="sidebar_list_link">
+          <li className="sidebar_list_item">
+            <i className="sidebar_list_item_icon fab fa-stack-overflow"></i>
+          </li>
+        </a>
+        <a href="https://dev.to" className="sidebar_list_link">
+          <li className="sidebar_list_item">
+            <i className="sidebar_list_item_icon fab fa-dev"></i>
+          </li>
+        </a>
+        <a href="https://linkedin.com" className="sidebar_list_link">
+          <li className="sidebar_list_item">
+            <i className="sidebar_list_item_icon fab fa-linkedin"></i>
+          </li>
+        </a>
+        <li id="feed" className="sidebar_list_item" onClick={activeLink}>
           <i className="sidebar_list_item_icon fas fa-rss-square"></i>
         </li>
-        <li className="sidebar_list_item">
+        <li id="settings" className="sidebar_list_item" onClick={activeLink}>
           <i className="sidebar_list_item_icon fas fa-cogs"></i>
         </li>
       </ul>
