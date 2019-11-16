2848465ce487b56ec5d3382da2e30808421c685c
Denis Onder <denis_onder@protonmail.com>
2019-11-16 20:07:31 +0100

    Pressing Space within the window will now auto-focus onto the search box

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import Hero from "./components/hero";
 import "./App.scss";
 
 function App() {
+  window.onkeydown = ({ target, keyCode }) => {
+    const input = document.getElementsByClassName("search_box")[0];
+    if (keyCode === 32 && target !== input) {
+      input.value = "";
+      input.focus();
+    }
+  };
   return (
     <div id="App" className="App">
       <Hero />
