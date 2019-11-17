151fe18253b8cb60fc50f720fa77f3e12bc681ac
Denis Onder <denis_onder@protonmail.com>
2019-11-17 01:09:27 +0100

    ESC clears focus from search box

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import "./App.scss";
 function App() {
   window.onkeydown = ({ target, keyCode }) => {
     const input = document.getElementsByClassName("search_box")[0];
+    // If space (code 32) is pressed, focus the box
     if (keyCode === 32 && target !== input) {
-      input.value = "";
       input.focus();
     }
+    // If ESC (code 27) is pressed, reset the focus
+    else if (keyCode === 27) input.blur();
   };
   return (
     <div id="App" className="App">
